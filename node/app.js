e2a0e55d59f75548a673420783b8e5d2ee3ff78a
olegarino <otchernov@gmail.com>
2018-03-24 15:34:55 -0400

    node api repo transfer

--- node/app.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-var db = require('./db');
-var PodcastController = require('./user/PodcastController');
-
-app.use('/podcast', PodcastController);
-
-module.exports = app;
--- node/db.js
+++
@@ -1,3 +0,0 @@
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://uri goes here');
